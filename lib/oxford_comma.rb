5b056e2acfabe17a815b2c7473b429612a8ebbf0
Kristen Papke <klpapke215@gmail.com>
2018-07-01 19:30:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,12 @@
 
-def oxford_comma(array)
-  array.join(", ")
+def oxford_comma(strings)
+  sentence = []
+  strings.each_with_index do |string, index|
+    if index < strings.length
+      sentence << string + ", "
+    elsif index == strings.length
+      sentence << " and " + string
+    end
+  end
+  sentence.join
 end
